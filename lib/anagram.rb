d7997f9df41dfe813c2fa9435e26b49586244ed6
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-28 21:04:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram 
+
+	@@all = [] 
+
+%w(hello world zombies pants dipper) = []
+	attr_accessor :anagram 
+
+def initialize (anagram) 
+
+	@anagram = anagram 
+	end 
+end
